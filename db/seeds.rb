1b990af217084bc8873480769af5cdb8fa4a7f98
Noemi Ashizuka <noemi.calanducci@gmail.com>
2020-11-03 12:56:17 +0900

    added other controller actions

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+
+Flat.create!(
+  name: 'Traditional Japanese Room',
+  address: 'Tokyo Asakusa',
+  description: 'Wood, tatami, drafts and mold. The true Japan experience',
+  price_per_night: 1000,
+  number_of_guests: 2
+)
+
+Flat.create!(
+  name: 'Modern Room in Tokyo',
+  address: 'Tokyo Shinjuku',
+  description: 'All the luxuries, conveniently located near one of the biggest stations in Tokyo',
+  price_per_night: 5000,
+  number_of_guests: 4
+)
